1be9f820906b7796dd39f3e0109d1c2a7272dfaa
INILARSION <lars.karbach@web.de>
2021-10-23 14:02:44 +0200

    Added new binaries, fixed high level compiler run code

--- Frontend_TM/views.py
+++ Frontend_TM/views.py
@@ -37,7 +37,7 @@ def handle_makros(program_string, session_id):
     file_name = str(base_path / "binaries/makro_file") + session_id
     result_file = str(base_path / "binaries/makro_file_result") + session_id
     create_file(file_name, program_string)
-    p = run([base_path / "binaries/makro_compiler", file_name, result_file], stdout=PIPE, encoding="ascii", stderr=PIPE)
+    p = run([base_path / "binaries/macro_compiler", file_name, result_file], stdout=PIPE, encoding="ascii", stderr=PIPE)
     remove_file(file_name)
     result = {
         "Returncode": p.returncode,
@@ -99,7 +99,7 @@ def handle_multitape_compiler(request_dict, session_id):
     tape_file = str(base_path / "binaries/multitape_tm_tape") + session_id
     create_file(program_file, request_dict["program"])
     create_file(tape_file, request_dict["tape"])
-    p = run([base_path / "binaries/multitape_tm", tape_file, program_file], stdout=PIPE, encoding="ascii", stderr=PIPE)
+    p = run([base_path / "binaries/multitape_compiler", tape_file, program_file], stdout=PIPE, encoding="ascii", stderr=PIPE)
     remove_file(program_file)
     remove_file(tape_file)
     return {
@@ -133,18 +133,22 @@ def handle_nondeterministic_tm(request_dict, session_id):
 
 
 def handle_high_level_tm(request_dict, session_id):
-    program_file = str(base_path / "binaries/nondeterministic_tm_prog") + session_id
-    output_file = str(base_path / "binaries/nondeterministic_tm_out") + session_id
+    program_file = str(base_path / "binaries/highlevel_tm_prog") + session_id
+    output_file = str(base_path / "binaries/highlevel_tm_out") + session_id
     create_file(program_file, request_dict["program"])
-    p = run([base_path / "binaries/high_level_compiler", "-v", program_file, output_file], stdout=PIPE, encoding="ascii", stderr=PIPE)
+    p = run([base_path / "binaries/high_level_compiler", program_file, output_file], stdout=PIPE, encoding="ascii", stderr=PIPE)
     result = {
         "Returncode": p.returncode,
-        "Error": p.stderr,
+        "Error": p.stdout,
     }
-    with open(output_file + ".delta", "r") as delta_file:
-        result["Output"] = delta_file.read()
-    with open(output_file + ".tape", "r") as tape_file:
-        result["Tape"] = tape_file.read()
+    if p.returncode == 0:
+        with open(output_file + ".delta", "r") as delta_file:
+            result["Output"] = delta_file.read()
+        with open(output_file + ".tape", "r") as tape_file:
+            result["Tape"] = tape_file.read()
+    else:
+        result["Output"] = p.stdout
+        result["Tape"] = ""
     remove_file(program_file)
     remove_file(output_file + ".delta")
     remove_file(output_file + ".tape")
